5590094639d39ac77aa76b15b57844dfdd01d9c0
kn2282 <57344058+kn2282@users.noreply.github.com>
2020-12-10 15:37:55 +0100

    bug fixed

--- src/utility/statistics.py
+++ src/utility/statistics.py
@@ -29,8 +29,8 @@ class Statistics:
     def number_of_nodes(self):
         return len(self.get_list_of_node())
     
-    def number_of_abcd_nodes(self):
-        return len(list(filter( lambda x: str(x.get_label()) in 'abcd', self.get_list_of_node())))
+    def number_of_abcd_nodes(self, label):
+        return len(list(filter( lambda x: str(x.get_label()) == label, self.get_list_of_node())))
 
     def number_of_edges(self):
         return len(self.graph.get_edge_list())
@@ -55,15 +55,15 @@ class Statistics:
     def averge_vertex_degree(self): 
         return round(self.number_of_edges() * 2 / self.number_of_nodes(),2)
     
-    def averge_vertex_degree_abcd(self):
-        if self.number_of_abcd_nodes() == 0: return 0 
-        counter = {str(x): 0 for x in 'abcd'}
+    def averge_vertex_degree_abcd(self, label):
+        if self.number_of_abcd_nodes(label) == 0: return 0 
+        counter = 0
         for edge in self.graph.get_edge_list():
             source_label = self.find_node_by_name(edge.get_source()).get_label()
             destination_label = self.find_node_by_name(edge.get_destination()).get_label()
-            if str(source_label) in 'abcd': counter[source_label] += 1
-            if str(destination_label) in 'abcd': counter[destination_label] += 1
-        return {str(i): round(counter[i]/self.number_of_abcd_nodes(),2) for i in counter.keys}
+            if str(source_label) == label: counter += 1
+            if str(destination_label) == label: counter += 1
+        return round(counter/self.number_of_abcd_nodes(label),2)
     
     def subgraphs_avarge_degree(self):
         return round(self.number_of_nodes() / self.number_of_subgraphs(),2)
@@ -74,10 +74,10 @@ class Statistics:
             StatisticsKeys.no_edges: self.number_of_edges(),
             StatisticsKeys.no_subgraphs: self.number_of_subgraphs(),
             StatisticsKeys.averge_vertex_degree: self.averge_vertex_degree(),
-            StatisticsKeys.averge_vertex_degree_a: self.averge_vertex_degree_abcd()['a'],
-            StatisticsKeys.averge_vertex_degree_b: self.averge_vertex_degree_abcd()['b'],
-            StatisticsKeys.averge_vertex_degree_c: self.averge_vertex_degree_abcd()['c'],
-            StatisticsKeys.averge_vertex_degree_d: self.averge_vertex_degree_abcd()['d'],
+            StatisticsKeys.averge_vertex_degree_a: self.averge_vertex_degree_abcd('a'),
+            StatisticsKeys.averge_vertex_degree_b: self.averge_vertex_degree_abcd('b'),
+            StatisticsKeys.averge_vertex_degree_c: self.averge_vertex_degree_abcd('c'),
+            StatisticsKeys.averge_vertex_degree_d: self.averge_vertex_degree_abcd('d'),
             StatisticsKeys.subgraphs_avarge_degree: self.subgraphs_avarge_degree(),
         }
 
